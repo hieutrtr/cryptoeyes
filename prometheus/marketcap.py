87e90d404ff31b4af41ead30c943fbf4ffdeb2aa
Hieu Tran <hieutt@adasiaholdings.com>
2018-01-11 10:33:43 +0700

    alert marketcap change

--- prometheus/marketcap.py
+++ prometheus/marketcap.py
@@ -4,9 +4,18 @@ import os.path,sys
 import datetime,time
 import requests
 from prometheus_client import start_http_server, Gauge
+from telegram.ext import Updater,CommandHandler
+
+FIVE_MIN = 300
+HAFL_HOUR = 1800
+HOUR = 3600
+DAY = HOUR*24
+
 dir_path = os.path.dirname(os.path.realpath(__file__))
 rose_host = os.environ['ROSE_HOST']
-start_http_server(8000)
+prom_host = os.environ['PROM_HOST']
+updater = Updater(token='464648319:AAFO8SGTukV4LHYtzpmjhbybyrwt0QQwIp8')
+job = updater.job_queue
 
 exchanges = ['marketcap']
 coins = ['bitcoin','ethereum','bitcoin-cash','iota','ripple','dash','litecoin']
@@ -21,11 +30,41 @@ partition = datetime.datetime.fromtimestamp(int(time.time())).strftime('%Y-%m-%d
 topics = tuple(ex + '.' + coin + '.' + partition for ex in exchanges for coin in coins)
 print(topics)
 consumer = KafkaConsumer(bootstrap_servers=rose_host,group_id='prometheus',auto_offset_reset='earliest')
-consumer.subscribe(topics=topics)
-for msg in consumer:
-    print(msg)
-    print(msg.value.decode('ascii'))
-    value = json.loads(msg.value.decode('ascii'))
-    payload = ''
-    for col in columns:
-        gauge_metrics[col].labels(value['id'].replace('-', '_'), value['name']).set(float(value.get(col,0.0)))
+
+def cap_check(coin_id,value):
+    ts = time.time()
+    r = requests.get(prom_host+'/api/v1/query?query=market_cap_usd{id="{}"}&time={}'.format(coin_id,int(ts)-FIVE_MIN))
+    if r.status_code >= 400: r.raise_for_status()
+    res = r.json()
+    last_vals = res["data"]["result"][0]["value"]
+    for lv in last_vals:
+        if type[lv] is str:
+            if float(lv) < float(value):
+                percent = ((float(value) - float(lv)) / float(lv)) * 100
+                if percent > 10:
+                    return '{} is increase {} percent of marketcap in 5 minutes : {}'.format(coin_id,percent,value)
+            elif float(lv) > float(value):
+                percent = ((float(lv) - float(value)) / float(lv)) * 100
+                if percent > 10:
+                    return '{} is decrease {} percent of marketcap in 5 minutes : {}'.format(coin_id,percent,value)
+    return None
+
+def cap_alert(bot, job):
+    start_http_server(8000)
+    consumer.subscribe(topics=topics)
+    for msg in consumer:
+        print(msg)
+        print(msg.value.decode('ascii'))
+        value = json.loads(msg.value.decode('ascii'))
+        payload = ''
+        coin_id = value['id'].replace('-', '_')
+        for col in columns:
+            metric_val = float(value.get(col,0.0))
+            if col == "market_cap_usd":
+                message = cap_check(coin_id,metric_val)
+                if message is not None:
+                    bot.send_message(chat_id='423404239',text=message)
+            gauge_metrics[col].labels(coin_id, value['name']).set(metric_val)
+
+job.run_repeating(cap_alert, interval=300, first=0)
+job.start()
