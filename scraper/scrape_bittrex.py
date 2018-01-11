d155d2a5a3a7268216899ebebbc05cfa29f2886a
Hieu Tran <hieutt@adasiaholdings.com>
2018-01-11 23:23:50 +0700

    use multi thread

--- scraper/scrape_bittrex.py
+++ scraper/scrape_bittrex.py
@@ -6,39 +6,49 @@ from kafka import KafkaProducer
 import json,datetime,time
 import redis
 from bittrex.bittrex import Bittrex, API_V2_0, API_V1_1, BUY_ORDERBOOK, TICKINTERVAL_ONEMIN, TICKINTERVAL_HOUR
-
-bittrex = Bittrex(os.environ['CRYPTOEYES_KEY'], os.environ['CRYPTOEYES_SEC'])
-bittrexv2 = Bittrex(os.environ['CRYPTOEYES_KEY'], os.environ['CRYPTOEYES_SEC'],api_version=API_V2_0)
+import thread
 
 rose_host = os.environ['ROSE_HOST']
-chunk = int(os.environ['CHUNK']) if os.environ['CHUNK'] is not None else 1
-producer = KafkaProducer(bootstrap_servers=rose_host)
 coinmarketcap = Market()
 # coins = ['bitcoin','ethereum','bitcoin-cash','iota','ripple','dash','litecoin']
 partition = datetime.datetime.fromtimestamp(int(time.time())).strftime('%Y-%m-%d')
-coins = coinmarketcap.ticker(limit=200)
+coins = coinmarketcap.ticker(limit=500)
 TICKINTERVAL = {
     "HOUR":TICKINTERVAL_HOUR,
     "ONEMIN": TICKINTERVAL_ONEMIN
 }
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
-for coin in coins[(chunk-1)*10:chunk*10]:
-    market = 'BTC-' + coin["symbol"]
-    if coin["symbol"] == 'BTC':
-        market = 'USDT-' + coin["symbol"]
-    histories = bittrex.get_market_history(market)
-    if histories.get("success") == True and histories.get("result") is not None:
-        hist_lenght = len(histories["result"])-1
-        topic = 'bittrex.' + market + '.history'
-        print(topic)
-        check_point = r.get(topic + '.check_point')
-        for i in range(hist_lenght,-1,-1):
-            hist = histories["result"][i]
-            if check_point is None or hist["Id"] > int(check_point):
-                producer.send(topic + '.' + partition, json.dumps(hist).encode())
-        r.set(topic+'.check_point',histories["result"][hist_lenght]["Id"])
-    else: print(market,histories)
-print("there're " + str(len(coins)) + " of coins are tracking.")
+def scrape(chunk=1):
+    producer = KafkaProducer(bootstrap_servers=rose_host)
+    for coin in coins[(chunk-1)*10:chunk*10]:
+        market = 'BTC-' + coin["symbol"]
+        if coin["symbol"] == 'BTC':
+            market = 'USDT-' + coin["symbol"]
+
+        bittrex = Bittrex(os.environ['CRYPTOEYES_KEY'], os.environ['CRYPTOEYES_SEC'])
+        bittrexv2 = Bittrex(os.environ['CRYPTOEYES_KEY'], os.environ['CRYPTOEYES_SEC'],api_version=API_V2_0)
+        histories = bittrex.get_market_history(market)
+        if histories.get("success") == True and histories.get("result") is not None:
+            hist_lenght = len(histories["result"])-1
+            topic = 'bittrex.' + market + '.history'
+            print(topic)
+            check_point = r.get(topic + '.check_point')
+            for i in range(hist_lenght,-1,-1):
+                hist = histories["result"][i]
+                if check_point is None or hist["Id"] > int(check_point):
+                    producer.send(topic + '.' + partition, json.dumps(hist).encode())
+            r.set(topic+'.check_point',histories["result"][hist_lenght]["Id"])
+        else: print(market,histories)
+    print("there're " + str(len(coins)) + " of coins are tracking.")
+
+# Create two threads as follows
+try:
+   for i in range(50):
+       thread.start_new_thread( scrape, (i, ) )
+except:
+   print "Error: unable to start thread"
+
+time.sleep(240)
 #
 # print("--------Ticker\n", bittrex.get_ticker("BTC-LTC"))
 # print("--------Sum\n", bittrex.get_marketsummary("BTC-LTC"))
