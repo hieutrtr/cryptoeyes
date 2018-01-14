28946b572f625e1aa004e8519ca46034416d5d48
hieutrtr <hieutrantrung.it@gmail.com>
2018-01-15 05:18:07 +0700

    sum btc

--- bots/cmd_bot.py
+++ bots/cmd_bot.py
@@ -147,8 +147,8 @@ def my_balance(bot, update, args):
                 sum_btc += ticker
             elif ba["Currency"] == 'BTC':
                 sum_btc += ba["Balance"]
-            message += '*{}*:{}({})\n'.format(ba["Currency"],ba["Balance"],ticker)
-    message+='Sum BTC: {}'.format(sum_btc)
+            message += '*{}*:{} ({})\n'.format(ba["Currency"],ba["Balance"],ticker)
+    message+='*Sum BTC*: {}'.format(sum_btc)
     bot.send_message(chat_id=update.message.chat_id, text=message,parse_mode=ParseMode.MARKDOWN)
 my_balance_handler = CommandHandler('mb', my_balance, pass_args=True)
 dispatcher.add_handler(my_balance_handler)
