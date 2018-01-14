ba6d0ecf24b24eb53c37ce1ca08fe445442ababc
hieutrtr <hieutrantrung.it@gmail.com>
2018-01-15 04:58:24 +0700

    + operation

--- bots/cmd_bot.py
+++ bots/cmd_bot.py
@@ -140,7 +140,8 @@ def my_balance(bot, update, args):
     message = ""
     for ba in bittrex.get_balances()["result"]:
         if ba["Balance"] != 0:
-            message += ba + "\n"
+            message += '{}\n'.format(ba)
+
     bot.send_message(chat_id=update.message.chat_id, text="My balances:{}".format(message),parse_mode=ParseMode.MARKDOWN)
 my_balance_handler = CommandHandler('mb', my_balance, pass_args=True)
 dispatcher.add_handler(my_balance_handler)
