53677e450b119db33f8214b9f276d796700ffa56
Hieu Tran <hieutt@adasiaholdings.com>
2018-01-20 09:40:53 +0700

    check my trans history

--- bots/cmd_bot.py
+++ bots/cmd_bot.py
@@ -205,7 +205,7 @@ my_balance_handler = CommandHandler('mb', my_balance, pass_args=True)
 dispatcher.add_handler(my_balance_handler)
 
 def my_trans(bot, update, args):
-    result = bittrex.get_order_history()["result"]
+    result = bittrex.get_order_history()["result"][:5]
     print(result)
     message = "{}".format(result)
     bot.send_message(chat_id=update.message.chat_id, text=message,parse_mode=ParseMode.MARKDOWN)
