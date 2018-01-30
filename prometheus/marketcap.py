5d6914bcd8b48b6b31dc3c421d83cac2d2d2f9ea
Hieu Tran <hieutt@adasiaholdings.com>
2018-01-30 15:47:55 +0700

    notify change of volume marketcap

--- prometheus/marketcap.py
+++ prometheus/marketcap.py
@@ -18,7 +18,7 @@ DAY = HOUR*24
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 rose_host = os.environ['ROSE_HOST']
-prom_host = os.environ['PROM_HOST']
+# prom_host = os.environ['PROM_HOST']
 tracking_limit = int(os.environ['TRACKING_LIMIT']) if os.environ['TRACKING_LIMIT'] is not None else 5
 my_chatid = os.environ['MY_CHATID']
 updater = Updater(token=os.environ['BOT_TOKEN'])
@@ -30,65 +30,65 @@ with open(dir_path+"/../config/coin_list.json") as coinListFile:
     coins = json.load(coinListFile)
     coinListFile.close()
 columns = ["market_cap_usd", "price_usd", "price_btc", "percent_change_24h","percent_change_7d", "percent_change_1h", "rank", "day_volume_usd"]
-gauge_metrics = dict()
-for col in columns:
-    gauge_metrics[col] = Gauge(col, 'Marketcap gauge data', ['id', 'name'])
-partition = datetime.datetime.fromtimestamp(int(time.time())).strftime('%Y-%m-%d')
+# gauge_metrics = dict()
+# for col in columns:
+#     gauge_metrics[col] = Gauge(col, 'Marketcap gauge data', ['id', 'name'])
+partition = datetime.datetime.fromtimestamp(int(time.time())).strftime('%Y')
 topics = tuple(ex + '.' + coin + '.' + partition for ex in exchanges for coin in coins)
 print(topics)
 consumer = KafkaConsumer(bootstrap_servers=rose_host,group_id='prometheus',auto_offset_reset='earliest')
 
-def cap_check(volume,symbol,coin_id,value):
-    ts = time.time()
-    url = 'http://' + prom_host+'/api/v1/query?query=market_cap_usd{id="%s"}&time=%d' % (coin_id,int(ts)-DAY/2,)
-    print("cap_check_url:",url)
-    r = requests.get(url=url)
-    if r.status_code >= 400: r.raise_for_status()
-    res = r.json()
-    last_vals = []
-    if len(res["data"]["result"]) > 0:
-        last_vals = res["data"]["result"][0]["value"]
-    for lv in last_vals:
-        if type(lv) is str:
-            if float(lv) < float(value):
-                percent = ((float(value) - float(lv)) / float(lv)) * 100
-                print("cap increase percent",percent)
-                if percent > tracking_limit:
-                    return '*{} ({})* is *increase {}*  percent of marketcap in 12 hours at price *{}* with 24h volume *{}*'.format(coin_id,symbol,percent,value,volume)
-            elif float(lv) > float(value):
-                percent = ((float(lv) - float(value)) / float(lv)) * 100
-                print("cap decrease percent",percent)
-                if percent > tracking_limit:
-                    return '*{}* is *decrease* {} percent of marketcap in 12 hours : {}'.format(coin_id,percent,value)
-    return None
-
-def price_check(coin_id,value):
-    ts = time.time()
-    if coin_id == 'bitcoin':
-        url = 'http://' + prom_host+'/api/v1/query?query=price_usd{id="%s"}&time=%d' % (coin_id,int(ts)-DAY/2,)
-    else:
-        url = 'http://' + prom_host+'/api/v1/query?query=price_btc{id="%s"}&time=%d' % (coin_id,int(ts)-DAY/2,)
-    print("price_check_url",url)
-    r = requests.get(url=url)
-    if r.status_code >= 400: r.raise_for_status()
-    res = r.json()
-    last_vals = []
-    if len(res["data"]["result"]) > 0:
-        last_vals = res["data"]["result"][0]["value"]
-    for lv in last_vals:
-        if type(lv) is str:
-            if float(lv) < float(value):
-                percent = ((float(value) - float(lv)) / float(lv)) * 100
-                print("price increase percent",percent)
-                return '---*{}* is *increase* {} percent of price in 12 hours : {}'.format(coin_id,percent,value)
-            elif float(lv) > float(value):
-                percent = ((float(lv) - float(value)) / float(lv)) * 100
-                print("price decrease percent",percent)
-                return '---*{}* is *decrease* {} percent of price in 12 hours : {}'.format(coin_id,percent,value)
-    return None
+# def cap_check(volume,symbol,coin_id,value):
+#     ts = time.time()
+#     url = 'http://' + prom_host+'/api/v1/query?query=market_cap_usd{id="%s"}&time=%d' % (coin_id,int(ts)-DAY/2,)
+#     print("cap_check_url:",url)
+#     r = requests.get(url=url)
+#     if r.status_code >= 400: r.raise_for_status()
+#     res = r.json()
+#     last_vals = []
+#     if len(res["data"]["result"]) > 0:
+#         last_vals = res["data"]["result"][0]["value"]
+#     for lv in last_vals:
+#         if type(lv) is str:
+#             if float(lv) < float(value):
+#                 percent = ((float(value) - float(lv)) / float(lv)) * 100
+#                 print("cap increase percent",percent)
+#                 if percent > tracking_limit:
+#                     return '*{} ({})* is *increase {}*  percent of marketcap in 12 hours at price *{}* with 24h volume *{}*'.format(coin_id,symbol,percent,value,volume)
+#             elif float(lv) > float(value):
+#                 percent = ((float(lv) - float(value)) / float(lv)) * 100
+#                 print("cap decrease percent",percent)
+#                 if percent > tracking_limit:
+#                     return '*{}* is *decrease* {} percent of marketcap in 12 hours : {}'.format(coin_id,percent,value)
+#     return None
+#
+# def price_check(coin_id,value):
+#     ts = time.time()
+#     if coin_id == 'bitcoin':
+#         url = 'http://' + prom_host+'/api/v1/query?query=price_usd{id="%s"}&time=%d' % (coin_id,int(ts)-DAY/2,)
+#     else:
+#         url = 'http://' + prom_host+'/api/v1/query?query=price_btc{id="%s"}&time=%d' % (coin_id,int(ts)-DAY/2,)
+#     print("price_check_url",url)
+#     r = requests.get(url=url)
+#     if r.status_code >= 400: r.raise_for_status()
+#     res = r.json()
+#     last_vals = []
+#     if len(res["data"]["result"]) > 0:
+#         last_vals = res["data"]["result"][0]["value"]
+#     for lv in last_vals:
+#         if type(lv) is str:
+#             if float(lv) < float(value):
+#                 percent = ((float(value) - float(lv)) / float(lv)) * 100
+#                 print("price increase percent",percent)
+#                 return '---*{}* is *increase* {} percent of price in 12 hours : {}'.format(coin_id,percent,value)
+#             elif float(lv) > float(value):
+#                 percent = ((float(lv) - float(value)) / float(lv)) * 100
+#                 print("price decrease percent",percent)
+#                 return '---*{}* is *decrease* {} percent of price in 12 hours : {}'.format(coin_id,percent,value)
+#     return None
 
 def cap_alert(bot, job):
-    start_http_server(8000)
+    # start_http_server(8000)
     consumer.subscribe(topics=topics)
     bittrex = Bittrex(os.environ['CRYPTOEYES_KEY'], os.environ['CRYPTOEYES_SEC'])
     check_dup = ""
@@ -111,7 +111,7 @@ def cap_alert(bot, job):
                 btc_last = bittrex.get_marketsummary("USDT-BTC")["result"][0]["Last"]
                 volume = float(value.get('24h_volume_usd',0.0))
                 btc_volume = volume/btc_last
-                if btc_volume > 5000:
+                if btc_volume > tracking_limit:
                     symbol = value.get('symbol',0.0)
                     cap = value.get("market_cap_usd",0.0)
                     if symbol == 'BTC':
@@ -135,7 +135,7 @@ def cap_alert(bot, job):
                     #     if message is not None:
                     #         print(message)
                     #         bot.send_message(chat_id=my_chatid,text=message,parse_mode=ParseMode.MARKDOWN)
-            gauge_metrics[col].labels(coin_id, value['name']).set(metric_val)
+            # gauge_metrics[col].labels(coin_id, value['name']).set(metric_val)
 
 job.run_repeating(cap_alert, interval=3600 * 24 * 60, first=0)
 job.start()
