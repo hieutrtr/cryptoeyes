dd0b0a3e2c22d2a61ae2fc2eb4c176eb1fe3938c
Hieu Tran <hieutt@adasiaholdings.com>
2018-02-12 14:03:38 +0700

    just send when reversing

--- workers/candle_watcher.py
+++ workers/candle_watcher.py
@@ -49,9 +49,9 @@ def analyze_candle(candle):
     up_tail = candle['H'] - candle['C'] if body >= 0 else candle['H'] - candle['O']
     low_tail = candle['O'] - candle['L'] if body >= 0 else candle['C'] - candle['L']
     body = (body * -1) if body < 0 else body
-    if up_tail >= body*2 and low_tail <= body/2:
+    if up_tail >= body*2:
         return candle
-    elif low_tail >= body*2 and up_tail <= body/2:
+    elif low_tail >= body*2:
         return candle
     return {}
 
